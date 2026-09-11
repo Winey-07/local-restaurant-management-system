24a9dce25e7b0f9fb8ec9547db38038cf701000b
powiney <powineyborin@gmail.com>
2026-09-11 18:29:42 +0700

    create table and menuItem migrations

---
+++ app/Models/menuItem.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class menuItem extends Model
+{
+    protected $fillable =[
+        'category_id',
+        'name',
+        'description',
+        'price',
+        'image',
+        'status',
+    ];
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+}
