360535a5d9c8f3a06343d480aeebe84ce6e5edde
powiney <powineyborin@gmail.com>
2026-09-15 00:36:16 +0700

    database done

--- database/migrations/2026_09_10_001234_create_user_login_table.php
+++ database/migrations/2026_09_10_001234_create_user_login_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->enum('role', ['admin', 'staff'])->default('staff');
             $table->rememberToken();
             $table->timestamps();
-        });`
+        });
     }
 
     /**
