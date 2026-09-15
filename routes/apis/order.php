ce5b1e09867b13a14c45155afd22db6b4af79717
Sreysoth <sreysothsamreth13@gmail.com>
2026-09-15 19:39:21 +0700

    add new route and api connection

---
+++ routes/apis/order.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\OrderController;
+
+Route::get ('/order', [OrderController::class, 'index']);
+Route::get ('/order/create', [OrderController::class, 'create']);
+Route::post('/order', [OrderController::class, 'store']);   
+Route::get ('/order/{id}', [OrderController::class, 'show']);
+Route::put ('/order/{id}', [OrderController::class, 'update']);
+Route::delete ('/order/{id}', [OrderController::class, 'destroy']);
