e3394227b27bcd0c3f7d7e3480e0a12c18042fe1
testing <reaksathol560@gmail.com>
2026-09-16 17:49:12 +0700

    edit api

--- routes/apis/category.php
+++ routes/apis/category.php
@@ -6,8 +6,8 @@ use App\Http\Controllers\CategoryController;
 
 Route::group(['prefix'=>'categories'], function(){
     Route::get('/', [CategoryController::class, 'index']);
-    Route::get('/{id}', [CategoryController::class], 'show');
-    Route::post('/', [CategoryController::class], 'store');
-    Route::put('/{id}', [CategoryController::class], 'update');
-    Route::delete('/{id}', [CategoryController::class], 'destory');
+    Route::get('/{id}', [CategoryController::class, 'show']);
+    Route::post('/', [CategoryController::class, 'store']);
+    Route::put('/{id}', [CategoryController::class, 'update']);
+    Route::delete('/{id}', [CategoryController::class, 'destory']);
 });
